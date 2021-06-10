7f796c9d7742a009ea4e1a041baa2584ded50df4
samhitha19wh1a12a5 <69640718+samhitha19wh1a12a5@users.noreply.github.com>
2021-06-10 19:16:31 +0530

    Add files via upload

---
+++ java programming lab/basic java programs/equals.java
@@ -0,0 +1,11 @@
+public class equals{
+public static void main(String[] args){
+String s1 = "Hello";
+String s2 = new String("Hello");
+if(s1.equals(s2))
+{System.out.println("s1 and s2 are equal");
+}
+else{
+System.out.println("s1 and s2 are not EQUAL");}
+}
+}
