2883507216aee04f23fd81ca0449e98b3cf41035
samhitha19wh1a12a5 <69640718+samhitha19wh1a12a5@users.noreply.github.com>
2021-06-10 19:18:26 +0530

    Add files via upload

---
+++ java programming lab/basic java programs/multiply.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;  
+class multiply_2_num
+{  
+public static void main(String[] args)   
+{  
+Scanner sc=new Scanner(System.in);  
+System.out.print("Enter any 2 number: ");   
+int a = sc.nextInt(); 
+int b = sc.nextInt();
+System.out.println("Multiplication of 2 numbers is "+ a*b);  
+}  
