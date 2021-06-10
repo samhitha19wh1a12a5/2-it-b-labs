ecadf2a7d2d06462960ba5a21c648997cfbba194
samhitha19wh1a12a5 <69640718+samhitha19wh1a12a5@users.noreply.github.com>
2021-06-10 19:31:31 +0530

    Add files via upload

---
+++ java programming lab/basic java programs/vowel.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class vowel{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter any character : ");
+    char ch = sc.next().charAt(0);
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch =='u'){
+       System.out.println( ch + " is vowel");}
+    else{
+       System.out.println(ch+" is consonant");}
+}
+}
