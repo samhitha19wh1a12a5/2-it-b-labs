8b6f0c34d60d015c690a4760d670b2962fa70ce2
samhitha19wh1a12a5 <69640718+samhitha19wh1a12a5@users.noreply.github.com>
2021-06-10 19:19:15 +0530

    Add files via upload

---
+++ java programming lab/basic java programs/power.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+import static java.lang.Math.pow;
+class power 
+{  
+public static void main(String[] args)   
+{  
+Scanner sc=new Scanner(System.in);  
+System.out.print("Enter a number: ");   
+int num = sc.nextInt(); 
+System.out.print("Enter the power: ");   
+int p = sc.nextInt(); 
+System.out.println("power of number is : "+Math.pow(num,p));  
+}  
+}  
+© 2021 GitHub, Inc.
