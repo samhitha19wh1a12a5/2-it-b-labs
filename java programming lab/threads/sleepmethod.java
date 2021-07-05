20d2ba9d04691dbc1d0cd4282cce8a71fc4e6bc0
samhitha19wh1a12a5 <69640718+samhitha19wh1a12a5@users.noreply.github.com>
2021-07-05 17:59:06 +0530

    Add files via upload

---
+++ java programming lab/threads/sleepmethod.java
@@ -0,0 +1,28 @@
+class A extends Thread
+{
+int i =1;
+public void run()
+{
+while(i<=10)
+{
+System.out.println("i = "+i);
+try
+{
+Thread.sleep(1000);
+}
+catch(Exception e)
+{
+}
+i++;
+}
+}
+}
+
+class Sleepmethod
+{
+public static void main(String args[])
+{
+A a = new A();
+a.start();
+}
+}
